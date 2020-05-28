641c58ef9d3d6631f3a9d307d16fee88ea06bd9f
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-05-28 15:29:41 +0200

    move verifyToken to service

--- src/main/java/at/fhv/teamb/symphoniacus/rest/service/AuthenticationService.java
+++ src/main/java/at/fhv/teamb/symphoniacus/rest/service/AuthenticationService.java
@@ -7,12 +7,16 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
 import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import java.io.File;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 public class AuthenticationService {
@@ -42,7 +46,7 @@ public class AuthenticationService {
      */
     public String issueToken(LoginUserDto user) throws JOSEException {
         JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-                .issuer("Symphoiacus")
+                .issuer("Symphoniacus")
                 .expirationTime(new Date(new Date().getTime() + 3600 * 4 * 1000))
                 .claim("username", user.getUserShortcut())
                 .claim("fullName", user.getFullName())
@@ -60,6 +64,35 @@ public class AuthenticationService {
     }
 
     /**
+     * Verifies a JWT token given as String.
+     *
+     * @param token JWT in compact string form
+     * @return SignedJWT token if valid, else empty Optioal
+     */
+    public Optional<SignedJWT> verifyToken(String token) {
+        boolean isValid;
+        String key = getKey();
+
+        try {
+            SignedJWT signedJwt = SignedJWT.parse(token);
+
+            JWSVerifier verifier = new MACVerifier(key);
+
+            isValid = signedJwt.verify(verifier)
+                    && (new Date().before(signedJwt.getJWTClaimsSet().getExpirationTime()));
+
+
+            if (isValid) {
+                return Optional.of(signedJwt);
+            }
+        } catch (ParseException | JOSEException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    /**
      * Reads the Keyfile from the resource folder once so it can be served.
      *
      * @return the Api key of Symphoniacus
@@ -68,7 +101,11 @@ public class AuthenticationService {
         if (this.key == null) {
             try {
                 ClassLoader classLoader = getClass().getClassLoader();
-                File keyFile = new File(classLoader.getResource("Api-Key.json").getFile());
+                File keyFile = new File(
+                        Objects.requireNonNull(
+                                classLoader.getResource("Api-Key.json")
+                        ).getFile()
+                );
 
                 ObjectMapper mapper = new ObjectMapper();
                 Map<?, ?> map = mapper.readValue(keyFile, Map.class);
