75b9dd971ef8b226718b7bc102c6ff3a8e067fc2
Valentin Goronjic <26229513+valentingc@users.noreply.github.com>
2020-05-28 16:39:35 +0200

    use global Response Object

--- src/main/java/at/fhv/teamb/symphoniacus/rest/api/DutyApi.java
+++ src/main/java/at/fhv/teamb/symphoniacus/rest/api/DutyApi.java
@@ -2,18 +2,20 @@ package at.fhv.teamb.symphoniacus.rest.api;
 
 import at.fhv.teamb.symphoniacus.application.dto.DutyDto;
 import at.fhv.teamb.symphoniacus.rest.configuration.jwt.Secured;
-import at.fhv.teamb.symphoniacus.rest.models.CustomResponse;
 import at.fhv.teamb.symphoniacus.rest.models.CustomResponseBuilder;
 import at.fhv.teamb.symphoniacus.rest.service.DutyService;
-import java.security.Principal;
-import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * API class for {@link DutyDto}.
@@ -28,7 +30,7 @@ public class DutyApi {
     /**
      * Get a specific Duty to a given Id.
      *
-     * @param id of the Duty to return.
+     * @param id              of the Duty to return.
      * @param securityContext to get the current Logged in UserId.
      * @return
      */
@@ -36,19 +38,30 @@ public class DutyApi {
     @Secured
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public CustomResponse getDuty(@PathParam("id") Integer id,
-                                  @Context SecurityContext securityContext) {
+    public Response getDuty(
+            @PathParam("id") Integer id,
+            @Context SecurityContext securityContext
+    ) {
         DutyDto duty = dutyService.getDuty(id);
 
-        if (duty != null) {
-            return new CustomResponseBuilder<DutyDto>("success", 200)
-                    .withPayload(duty)
-                    .build();
-        }
-
-        return new CustomResponseBuilder<Void>("Client Failure", 404)
-                .withMessage("Duty not Found")
+        if (duty == null) {
+            return Response
+                .status(Response.Status.BAD_REQUEST)
+                .type("text/json")
+                .entity(new CustomResponseBuilder<Void>("Client Failure", 404)
+                    .withMessage("Duty not Found")
+                    .build()
+                )
                 .build();
+        }
+        return Response
+            .status(Response.Status.OK)
+            .type("text/json")
+            .entity(new CustomResponseBuilder<DutyDto>("success",200)
+                .withPayload(duty)
+                .build()
+            )
+            .build();
     }
 
     /**
@@ -60,21 +73,22 @@ public class DutyApi {
     @Secured
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
-    public CustomResponse getAllDuties(@Context SecurityContext securityContext) {
+    public Response getAllDuties(@Context SecurityContext securityContext) {
         //To get the current logged in Username
         Principal principal = securityContext.getUserPrincipal();
         String username = principal.getName();
 
-        Set<DutyDto> duties = dutyService.getAllDuties(Integer.valueOf(username));
-
-        if (duties != null) {
-            return new CustomResponseBuilder<Set<DutyDto>>("success", 200)
-                    .withPayload(duties)
-                    .build();
+        Set<DutyDto> duties = this.dutyService.getAllDuties(Integer.valueOf(username));
+        if (duties == null) {
+            duties = new HashSet<>();
         }
-
-        return new CustomResponseBuilder<Void>("Client Failure", 400)
-                .withMessage("No Duties Found")
-                .build();
+        return Response
+            .status(Response.Status.OK)
+            .type("text/json")
+            .entity(new CustomResponseBuilder<Set<DutyDto>>("success",200)
+                .withPayload(duties)
+                .build()
+            )
+            .build();
     }
 }
