b3809b7485f54c4c78dbcc609fb1c0049ae6d623
1b0t <ibot@c0re.at>
2020-05-29 12:49:49 +0200

    Refactor Dtos

--- src/main/java/at/fhv/teamb/symphoniacus/rest/models/wish/DateWishDto.java
+++ src/main/java/at/fhv/teamb/symphoniacus/rest/models/wish/DateWishDto.java
@@ -3,16 +3,24 @@ package at.fhv.teamb.symphoniacus.rest.models.wish;
 import java.time.LocalDateTime;
 
 /**
- * Model class for {@link DateWish}.
+ * Model class for {@link DateWishDto}.
  *
  * @author Tobias Moser
  */
-public class DateWish {
+public class DateWishDto {
     private LocalDateTime start;
     private LocalDateTime end;
 
-    public DateWish(LocalDateTime start, LocalDateTime end) {
+    public DateWishDto(LocalDateTime start, LocalDateTime end) {
         this.start = start;
+        this.end = end;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public void setEnd(LocalDateTime end) {
         this.end = end;
     }
 
---
+++ src/main/java/at/fhv/teamb/symphoniacus/rest/models/wish/DutyWishDto.java
@@ -0,0 +1,56 @@
+package at.fhv.teamb.symphoniacus.rest.models.wish;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * API class for {@link DutyWishDto}.
+ *
+ * @author Tobias Moser
+ */
+public class DutyWishDto {
+    private Integer dutyId;
+    private Map<Integer, String> musicalPieces;
+    private Boolean forEntireSop;
+
+    public DutyWishDto(Integer dutyId, Boolean forEntireSop) {
+        this.dutyId = dutyId;
+        this.forEntireSop = forEntireSop;
+    }
+
+    /**
+     * Add a muscialPiece to a Dutywish.
+     *
+     * @param musicalPieceId to add
+     */
+    public void addMusicalPiece(Integer musicalPieceId, String name) {
+        if (musicalPieces == null) {
+            musicalPieces = new HashMap<>();
+        }
+        musicalPieces.put(musicalPieceId, name);
+    }
+
+    public void setDutyId(Integer dutyId) {
+        this.dutyId = dutyId;
+    }
+
+    public void setMusicalPieces(Map<Integer, String> musicalPieces) {
+        this.musicalPieces = musicalPieces;
+    }
+
+    public void setForEntireSop(Boolean forEntireSop) {
+        this.forEntireSop = forEntireSop;
+    }
+
+    public Integer getDutyId() {
+        return dutyId;
+    }
+
+    public Map<Integer, String> getMusicalPieces() {
+        return musicalPieces;
+    }
+
+    public Boolean getForEntireSop() {
+        return forEntireSop;
+    }
+}
